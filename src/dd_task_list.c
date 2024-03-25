37ab620665bdf71ac43a8c764805412440b0dcec
J-Raymer <joeygatacre4@gmail.com>
2024-03-24 18:39:53 -0700

    rename task list files

--- src/dd_task_list.c
+++ src/dd_task_list.c
@@ -2,49 +2,43 @@
 #include <stdlib.h>
 #include "stm32f4_discovery.h"
 #include "../FreeRTOS_Source/include/task.h"
-#include "list_operations.h"
+#include "dd_task_list.h"
 
 typedef enum task_type
 {
-	PERIODIC,
-	APERIODIC
+    PERIODIC,
+    APERIODIC
 } task_type;
 
 typedef struct dd_task
 {
-	TaskHandle_t t_handle;
-	task_type type;
-	uint32_t task_id;
-	uint32_t release_time;
-	uint32_t absolute_deadline;
-	uint32_t completion_time;
+    TaskHandle_t t_handle;
+    task_type type;
+    uint32_t task_id;
+    uint32_t release_time;
+    uint32_t absolute_deadline;
+    uint32_t completion_time;
 } dd_task;
 
 typedef struct dd_task_node
 {
-	dd_task task;
-	struct dd_task_node *next_task;
+    dd_task task;
+    struct dd_task_node *next_task;
 
 } dd_task_node;
 
 void insertAtFront()
 {
     dd_task task;
-    struct dd_task_node  *temp;
+    struct dd_task_node *temp;
     temp = malloc(sizeof(struct node));
     printf("\nEnter number to"
-        " be inserted : ");
+           " be inserted : ");
     scanf("%d", &data);
     temp->info = data;
- 
+
     // Pointer of temp will be
     // assigned to start
     temp->link = start;
     start = temp;
 }
-
-
-
-
-
-
